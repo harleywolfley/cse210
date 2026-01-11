09a6da21681d380d007e43e88a11a47fd7f01cab
harleywolfley <harleywolfley@gmail.com>
2026-01-10 19:18:53 -0700

    You are Bond, James Bond.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.Write("First Name: ");
+        string fname = Console.ReadLine();
+        Console.Write("Last Name: ");
+        string lname = Console.ReadLine();
+        Console.WriteLine($"You are {lname}, {fname} {lname}.");
     }
 }
