80f4553c809e11ee14df83e0721644c82aabcf1d
harleywolfley <harleywolfley@gmail.com>
2026-01-10 19:54:44 -0700

    Your grade is an F.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? \n");
+        string enteredGrade = Console.ReadLine();
+        int grade = int.Parse(enteredGrade);
+        if (grade >= 90)
+        {
+            Console.WriteLine("Your Grade is an A.");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("Your Grade is a B.");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("Your Grade is a C.");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("Your Grade is a D.");
+        }
+        else
+        {
+            Console.WriteLine("Your Grade is an F.");
+        }
     }
 }
